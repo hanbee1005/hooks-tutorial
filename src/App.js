ec1e1774ac85c618523606416d59b1dc8c950045
hanbee <sonhanbi1002@gmail.com>
2021-07-10 14:41:35 +0900

    useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { useState } from "react";
 import Counter from "./Counter";
 import Info from "./Info";
 
 function App() {
 
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
       <Counter />
-      <Info />
+      <div>
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? '숨기기' : '보이기'}
+        </button>
+        <hr />
+        {visible && <Info />}
+      </div>
     </div>
   );
 }
--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    // 마운트, 업데이트 모두 사용
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다.');
+        console.log({
+            name,
+            nickname
+        });
+    });
+
+    // 마운트될 때만 실행
+    useEffect(() => {
+        console.log('마운트될 때만 실행됩니다.');
+    }, []);
+
+    // 특정 값이 변경될 때만 사용 (name)
+    useEffect(() => {
+        console.log(name);
+    }, [name]);
+
+    // 언마운트되기 전이나 업데이트 되기 전 실행
+    // useEffect(() => {
+    //     console.log('update');
+    //     console.log(name);
+    //     return () => {
+    //         console.log('cleanup');
+    //         console.log(name);
+    //     };
+    // });
+
+    // 언마운트될 때만 실행
+    useEffect(() => {
+        console.log('update');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    }, []);
 
     const onChnageName = (e) => {
         setName(e.target.value);
