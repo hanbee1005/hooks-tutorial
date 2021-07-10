3eefaebbbf104a532507e62afa6a5dfbd491cef0
hanbee <sonhanbi1002@gmail.com>
2021-07-10 15:42:38 +0900

    useRef 사용

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중...');
@@ -10,6 +10,18 @@ const getAverage = numbers => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
+    const inputEl = useRef(null);
+
+    // 렌더링과 관련되지 않는 값 변경에 사용 가능
+    const localVar = useRef(1);
+
+    const setLocalVar = (n) => {
+        localVar.current = n;
+    }
+
+    const printLocalVar = () => {
+        console.log(localVar.current);
+    }
 
     // 컴포넌트가 처음 렌더링될 때만 함수 생성
     const onChange = useCallback(e => {
@@ -21,13 +33,14 @@ const Average = () => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
+        inputEl.current.focus();
     }, [number, list]);
 
     const avg = useMemo(() => getAverage(list), [list]);
 
     return (
         <div>
-            <input value={number} onChange={onChange} />
+            <input value={number} onChange={onChange} ref={inputEl} />
             <button onClick={onInsert}>등록</button>
             <ul>
                 {list.map((value, index) => {
